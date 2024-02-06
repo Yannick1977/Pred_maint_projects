0367cffec872e73e4ba4d2d9dd486489c6747f15
YannickB <68849596+Yannick1977@users.noreply.github.com>
2024-02-06 21:40:49 +0100

    wwwwwww

--- Src/__init__.py
+++ Src/__init__.py
@@ -3,7 +3,7 @@ import sys
 import logging
 
 logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
-
+print(f'folder : {os.getcwd()}')
 log_dir = "logs"
 log_filepath = os.path.join(log_dir,"running_logs.log")
 os.makedirs(log_dir, exist_ok=True)
