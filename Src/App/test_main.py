bb058167cda1bb7e0919cdce4ff96bfc6a82ea38
YannickB <68849596+Yannick1977@users.noreply.github.com>
2024-03-21 19:04:28 +0100

    Ajouter des tests pour les methode predict et explain

--- Src/App/test_main.py
+++ Src/App/test_main.py
@@ -1,9 +1,11 @@
+# Test the main.py file with the following test_main.py file:
 import os
 import sys
+from typing import List
 sys.path.insert(0, os.getcwd())
 import pytest
 from fastapi.testclient import TestClient
-from main import app, ItemInput
+from main import app, ItemInput, ItemOutputPredict
 
 
 client = TestClient(app)
@@ -14,7 +16,39 @@ def test_get_index():
     assert response.json() == "hello world !"
 
 def test_post_predict():
-    item = ItemInput(Type="M", Air_temperature=1, Process_temperature=1, Rotational_speed=1, Torque=1, Tool_wear=1, Difference_temperature=1, Power=1)
+    item = ItemInput(Type="M", 
+                     Air_temperature=295.3, 
+                     Process_temperature=305.7, 
+                     Rotational_speed=1168, 
+                     Torque=3.8, 
+                     Tool_wear=253
+                     #Difference_temperature=1, Power=1
+                     )
     response = client.post("/predict", json=item.dict())
     assert response.status_code == 200
-    assert "prediction" in response.json()
+    assert isinstance(response.json(), list)
+    assert len(response.json()) == 5
+    assert isinstance(response.json()[0]['proba'], float)
+    assert isinstance(response.json()[0]['name'], str)
+    assert all(isinstance(item, dict) for item in response.json())
+
+def test_post_explain():
+    item = ItemInput(Type="M",
+                     Air_temperature=295.3,
+                     Process_temperature=305.7,
+                     Rotational_speed=1168,
+                     Torque=3.8,
+                     Tool_wear=253
+                     #Difference_temperature=1, Power=1
+                     )    
+    response = client.post("/explain", json=item.dict())
+    assert response.status_code == 200
+    print(10*"!")
+    print(response.json())
+    assert isinstance(response.json(), list)
+    assert len(response.json()) == 5
+    assert isinstance(response.json()[0]['weight'], float)
+    assert isinstance(response.json()[0]['item'], str)
+    assert all(isinstance(item, dict) for item in response.json())
+    
+    
